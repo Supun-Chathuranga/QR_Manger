9950b8556deecf4ee653fe63f09e9cb6e3a3d6d6
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-08-17 20:48:30 +0530

    Update view_qr_code_screen.dart

---
+++ lib/view_qr_code_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'vehicle.dart'; // Import your Vehicle class or wherever you defined it
+
+class ViewQRCodeScreen extends StatelessWidget {
+  final Vehicle vehicle;
+
+  ViewQRCodeScreen({required this.vehicle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('View QR Code')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(vehicle.name),
+            SizedBox(height: 20),
+            // Replace with an Image widget that loads the QR code image
+            // For example:
+            // Image.network(vehicle.qrCodeImageUrl),
+          ],
+        ),
+      ),
+    );
+  }
+}
